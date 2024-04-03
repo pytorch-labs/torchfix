ec242539f7b44c6c63e7d1c3517133102e307c39
Francesca Wang <cesca@meta.com>
2024-04-03 14:26:31 -0700

    remove comment

--- torchfix/visitors/vision/models_import.py
+++ torchfix/visitors/vision/models_import.py
@@ -14,7 +14,6 @@ class TorchVisionModelsImportVisitor(TorchVisitor):
     def visit_Import(self, node: cst.Import) -> None:
         replacement = None
         for imported_item in node.names:
-            #  Check matches on each imported items from node
             if m.matches(imported_item, m.ImportAlias(
                 name=m.Attribute(value=m.Name("torchvision"),
                                     attr=m.Name("models")),
