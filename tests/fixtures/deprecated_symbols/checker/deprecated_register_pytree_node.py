b9468b94a6504acf2f3d7cd9ba11ea95c167fa53
Zain Rizvi <ZainR@meta.com>
2024-04-22 12:30:01 -0500

    Add rule for deprecated _register_pytree_node

---
+++ tests/fixtures/deprecated_symbols/checker/deprecated_register_pytree_node.py
@@ -0,0 +1,9 @@
+from torch.utils._pytree import _register_pytree_node
+
+_register_pytree_node()
+
+from torch.utils import _pytree as xx
+xx._register_pytree_node()
+
+import torch.utils._pytree._register_pytree_node as yy
+yy()
