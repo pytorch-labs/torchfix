1f98c1db0824a9e9e535bebb1c45200439110a42
Sergii Dymchenko <sdym@fb.com>
2024-09-17 14:51:42 -0700

    Add a negative test

--- tests/fixtures/misc/checker/expm1.py
+++ tests/fixtures/misc/checker/expm1.py
@@ -7,3 +7,6 @@ ret = (torch.exp(a) - 1) * torch.exp(2 * b)
 
 # False negative: can not detect currently
 x = a.exp() - 1
+
+# False negative: should be rare and would complicate implementation
+x = -1 + torch.exp(a)
