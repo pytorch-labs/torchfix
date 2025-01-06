f72111a2ba9bde3e8639f57970089ba08d7ec070
Sergii Dymchenko <sdym@fb.com>
2025-01-06 15:59:01 -0800

    More tests

--- tests/fixtures/misc/checker/logsumexp.py
+++ tests/fixtures/misc/checker/logsumexp.py
@@ -9,3 +9,6 @@ y = torch.log(torch.sum(torch.exp(2.5 + x), 1))
 # not logsumexp
 y = torch.log(torch.sum(torch.exp(x), 1, keepdim=True) + 2.5)
 y = torch.log(torch.sum(torch.exp(x) + 2.5, 1))
+y = torch.log(2 + x)
+y = torch.sum(torch.log(torch.exp(x)), 1)
+y = torch.exp(torch.sum(torch.log(x), 1, keepdim=True))
