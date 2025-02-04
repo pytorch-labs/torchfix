00954c918e3e6b46b1f55b7d1eb16f22bf4e6a3d
Sergii Dymchenko <sdym@meta.com>
2025-02-03 18:59:17 -0800

    Don't suggest logsumexp if sum's dim is None (#91)
    See https://github.com/pytorch/pytorch/issues/144339

--- tests/fixtures/misc/checker/logsumexp.py
+++ tests/fixtures/misc/checker/logsumexp.py
@@ -1,10 +1,12 @@
 import torch
-a = torch.randn(5)
-b = torch.randn(5)
+
+x = torch.randn(5)
 
 # logsumexp
 y = torch.log(torch.sum(torch.exp(x), 1, keepdim=True))
+y = torch.log(torch.sum(torch.exp(x), dim=1, keepdim=True))
 y = torch.log(torch.sum(torch.exp(2.5 + x), 1))
+y = torch.log(torch.sum(torch.exp(2.5 + x), dim=1))
 
 # not logsumexp
 y = torch.log(torch.sum(torch.exp(x), 1, keepdim=True) + 2.5)
@@ -12,3 +14,8 @@ y = torch.log(torch.sum(torch.exp(x) + 2.5, 1))
 y = torch.log(2 + x)
 y = torch.sum(torch.log(torch.exp(x)), 1)
 y = torch.exp(torch.sum(torch.log(x), 1, keepdim=True))
+
+# not logsumexp because of https://github.com/pytorch/pytorch/issues/144339
+y = torch.log(torch.sum(torch.exp(x), None, keepdim=True))
+y = torch.log(torch.sum(torch.exp(x), dim=None, keepdim=True))
+y = torch.log(torch.sum(torch.exp(x), keepdim=True))
